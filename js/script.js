df0ad8e68f0bc77d74b9e1bc2b595c4aef3644e7
KohCharles <charles@creatual.io>
2023-06-04 16:25:42 +0800

    mute tick buttons

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-//  document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.sidenav');
-//     var instances = M.Sidenav.init(elems, options);
-//   });
-
+// initialize functions
 $(document).ready(function () {
     $('.sidenav').sidenav();
     $('.dropdown-trigger').dropdown();
@@ -12,22 +8,17 @@ $(document).ready(function () {
 });
 
 
-// remove pulse effect after...
+// completed buttons fade out
 document.querySelector('#btn-confirm-1').addEventListener('click', () => {
- 
-
-    document.querySelector('#m-color-dis-1').classList.add('disabled');
-    document.querySelector('#m-color-dis-2').classList.add('disabled');
-    document.querySelector('#m-color-dis-3').classList.add('disabled');
-    document.querySelector('#m-color-dis-4').classList.add('disabled');
-    document.querySelector('#m-color-dis-5').classList.add('disabled');
-    document.querySelector('#m-color-dis-6').classList.add('disabled');
-
     document.querySelector('#btn-completed-1').classList.add('disabled');
 });
 
-// disable btn pickup at lcc -->
+document.querySelector('#btn-confirm-2').addEventListener('click', () => {
+    document.querySelector('#btn-completed-2').classList.add('disabled');
+});
 
+
+// LCC Green buttons
 document.querySelector('#btn-lcc1-up-confirm-1').addEventListener('click', () => {
     document.querySelector('#icc1-btn-1').classList.add('disabled');
 });
@@ -36,50 +27,88 @@ document.querySelector('#btn-lcc2-up-confirm-1').addEventListener('click', () =>
     document.querySelector('#icc2-btn-2').classList.add('disabled');
 });
 
-
-// display tick! -->
-
-document.querySelector('#btn-confirm-up-1').addEventListener('click', () => {
-    document.getElementById("btn-up-1").innerHTML = "done";
+// LCC Red buttons
+document.querySelector('#btn-icc2-drop-confirm-1').addEventListener('click', () => {
+    document.querySelector('#icc-btn-3').classList.add('disabled');
 });
 
-document.querySelector('#btn-confirm-off-1').addEventListener('click', () => {
-    document.getElementById("btn-off-1").innerHTML = "done";
-});
-
-document.querySelector('#btn-confirm-up-2').addEventListener('click', () => {
-    document.getElementById("btn-up-2").innerHTML = "done";
-});
-
-document.querySelector('#btn-confirm-off-2').addEventListener('click', () => {
-    document.getElementById("btn-off-2").innerHTML = "done";
-});
-
-document.querySelector('#btn-confirm-up-3').addEventListener('click', () => {
-    document.getElementById("btn-up-3").innerHTML = "done";
-});
-
-document.querySelector('#btn-confirm-off-3').addEventListener('click', () => {
-    document.getElementById("btn-off-3").innerHTML = "done";
+document.querySelector('#btn-icc1-drop-confirm-1').addEventListener('click', () => {
+    document.querySelector('#icc-btn-4').classList.add('disabled');
 });
 
 
+// tick GREEN buttons effects
 
-// document.querySelector('#modal-btn-2').addEventListener('click', () => {
-//     document.querySelector('#btn-wave-2').classList.remove('pulse');
+// green button 1 -->
+var mBtn1 = document.getElementById("btn-confirm-off-1");
+var tickBtn1 = document.getElementById("btn-off-1");
+var colorBtn1 = document.getElementById("m-color-dis-2");
 
-//     document.querySelector('#btn-wave-2').classList.add('btn-floating-disabled');
+mBtn1.addEventListener("click", tickFunction1);
 
-//     document.querySelector('#btn-done-2').classList.add('disabled');
-// });
+function tickFunction1() {
+    tickBtn1.innerHTML = "done"; 
+    colorBtn1.classList.add('disabled');       
+}
 
-// document.querySelector('#modal-btn-3').addEventListener('click', () => {
-//     document.querySelector('#btn-wave-3').classList.remove('pulse');
+// green button 2 -->
+var mBtn2 = document.getElementById("btn-confirm-off-2");
+var tickBtn2 = document.getElementById("btn-off-2");
+var colorBtn2 = document.getElementById("m-color-dis-4");
 
-//     document.querySelector('#btn-wave-3').classList.add('btn-floating-disabled');
+mBtn2.addEventListener("click", tickFunction2);
+  
+function tickFunction2() {
+    tickBtn2.innerHTML = "done"; 
+    colorBtn2.classList.add('disabled');       
+}
 
-//     document.querySelector('#btn-done-3').classList.add('disabled');
-// });
+// green button 3 -->
+var mBtn3 = document.getElementById("btn-confirm-off-3");
+var tickBtn3 = document.getElementById("btn-off-3");
+var colorBtn3 = document.getElementById("m-color-dis-6");
 
+mBtn3.addEventListener("click", tickFunction3);
 
+function tickFunction3() {
+    tickBtn3.innerHTML = "done"; 
+    colorBtn3.classList.add('disabled');       
+}
 
+// tick RED buttons effects
+
+// red button 1 -->
+var mBtn4 = document.getElementById("btn-confirm-up-1");
+var tickBtn4 = document.getElementById("btn-up-1");
+var colorBtn4 = document.getElementById("m-color-dis-1");
+
+mBtn4.addEventListener("click", tickFunction4);
+
+function tickFunction4() {
+    tickBtn4.innerHTML = "done"; 
+    colorBtn4.classList.add('disabled');       
+}
+
+// red button 2 -->
+var mBtn5 = document.getElementById("btn-confirm-up-2");
+var tickBtn5 = document.getElementById("btn-up-2");
+var colorBtn5 = document.getElementById("m-color-dis-3");
+
+mBtn5.addEventListener("click", tickFunction5);
+
+function tickFunction5() {
+    tickBtn5.innerHTML = "done"; 
+    colorBtn5.classList.add('disabled');       
+}
+
+// red button 3 -->
+var mBtn6 = document.getElementById("btn-confirm-up-3");
+var tickBtn6 = document.getElementById("btn-up-3");
+var colorBtn6 = document.getElementById("m-color-dis-5");
+
+mBtn6.addEventListener("click", tickFunction6);
+
+function tickFunction6() {
+    tickBtn6.innerHTML = "done"; 
+    colorBtn6.classList.add('disabled');       
+}
